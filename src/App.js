e80cffee38945bb3831c904f8a3f5df151b9c317
ducklin404 <duonght404@gmail.com>
2025-03-21 15:51:12 +0700

    adjust css a bit so it look better

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <img className="logo" src="./duck.webp" alt="duck logo" />
       <h1>A Silent Pond</h1>
       <div id="description">
-        <p>Hey, I'm Duck. Hope these resource could help you!</p>
+        <p>Hey, I'm Duck. Hope these resources could help you!</p>
       </div>
 
       <div id="projects-display">
